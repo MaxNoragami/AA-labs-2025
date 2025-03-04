be71e98cce14a44140b4352c3a6f76c909ddc871
MaxNoragami <maxcraftmd@gmail.com>
2025-03-04 10:26:37 +0200

    Feat: implemented four sorting algorithms

---
+++ lab2/quick_sort.py
@@ -0,0 +1,19 @@
+def partition(array, low, high):
+    # choose the rightmost element as pivot
+    pivot = array[high]
+
+    # pointer for greater element
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+
+def quick_sort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
